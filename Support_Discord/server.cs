7cb207087439671a30c7a6f6e10e67352ca79307
Lake <contact@lakeys.net>
2020-07-05 21:53:22 -0400

    Create initial link in Blockland

--- Support_Discord/server.cs
+++ Support_Discord/server.cs
@@ -1,1 +1,26 @@
-echo("BL-Discord 0.1.0");
+function initDiscordLink() {
+  if(!isObject(DiscordLink)) {
+    %link = new TCPObject(DiscordLink);
+  }
+
+  DiscordLink.connect("127.0.0.1:25625");
+}
+
+initDiscordLink();
+
+function DiscordLink::onConnected(%link) {
+  echo("Discord bridge connected.");
+  %link.send("{ type: \"Handshake\"}");
+}
+
+function DiscordLink::onLine(%link) {
+  echo("line");
+}
+
+function DiscordLink::onConnectFailed(%link) {
+  echo("Discord bridge connection failed.");
+}
+
+function DiscordLink::onDisconnect(%link) {
+  echo("Discord bridge disconnected.");
+}
