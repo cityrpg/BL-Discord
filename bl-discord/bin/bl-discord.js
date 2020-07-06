bffba9613efbbccbdd6e3cc791ead65ebf857913
Lake <contact@lakeys.net>
2020-07-05 21:26:48 -0400

    Create initial net server in bl-discord.js
    Create a simple net server to send and receive information from the game.

--- bl-discord/bin/bl-discord.js
+++ bl-discord/bin/bl-discord.js
@@ -1,7 +1,43 @@
 "use strict";
 
+// Requirements
+const net = require("net");
+
+// Init
 process.title = `Blockland-Discord`;
 var pkg = require("../package.json");
 console.log("BL-Discord " + pkg.version);
 
-//var Config = require("./config.js").config;
+var socket;
+
+const server = net.createServer((conn) => {
+  var addressInfo = conn.address();
+
+  // Local connections only
+  if(addressInfo.address !== "::ffff:127.0.0.1") {
+    conn.destroy();
+    return;
+  }
+  conn.pipe(conn);
+  console.log("Received connection from Blockland");
+
+  conn.write("Init");
+
+  if(typeof socket !== "undefined") {
+    socket.destroy();
+    console.log("Received new connection, closing the old one...");
+  }
+
+  conn.on("data", (buffer) => {
+    console.log(buffer);
+    console.log(buffer.toString());
+  });
+});
+
+server.on("error", (err) => {
+    throw err;
+});
+
+server.listen(25625, () => {
+  console.log("Server listening");
+});
